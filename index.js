084549c190cffa657d72d419835e567fd2d1432e
Paul von Hagen <pvonhagen26@gmail.com>
2020-09-08 01:17:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ class Circle {
   }
 
   const pi = Math.PI;
-  const diameter = this.radius * 2
+  const diameter = this.radius * 2;
+  const rsquared = this.radius ** 2;
 
   get diameter(){
     return this.radius * 2
